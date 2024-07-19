c859edacce7b6a42cc6f991ab3633f7b0d21c9fc
Mikiyas Alehegn <mikiyas.alehegn@linearb.io>
2024-07-19 20:34:49 +0300

    final verification

---
+++ gitPrac.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+CHECK_FOLDER=$(ls | grep Pages)
+
+CONTENT="<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Practice, Current date and time: $(date)</title>
+</head>
+<body>
+    
+</body>
+</html>"
+
+#!/bin/bash
+
+if [ "$CHECK_FOLDER" = "Pages" ]; then
+    cd "$CHECK_FOLDER"
+    touch "index.html"
+    echo "$CONTENT" > "index.html"
+    git add .
+    gcom "File created"
+    gps
+else
+    git clone https://github.com/mikiyasalehegn/gitPractice.git
+    cd "$CHECK_FOLDER"
+    touch "index.html"
+    echo "$CONTENT" > "index.html"
+    git add .
+    gcom "File created"
+    gps
+fi
